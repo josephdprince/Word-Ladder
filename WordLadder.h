3330565f9f9ca2a75243b487352a4d19c1595e42
josephdprince <57415450+josephdprince@users.noreply.github.com>
2021-09-09 11:18:45 -0700

    Update WordLadder.h

--- WordLadder.h
+++ WordLadder.h
@@ -10,12 +10,9 @@ class WordLadder {
  private:
    list<string> dict;
    int comparisons = 0;
-   bool optimization = false;
  public:
    WordLadder(const string &);
    void outputLadder(const string &, const string &, const string &);
-   void setOptimization(bool);
-   int getComparisons() const { return this->comparisons; }
  private:
    void PlayWordLadder(const string &, const string &, const string &, ofstream &);
 };
