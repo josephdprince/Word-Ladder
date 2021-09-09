0eeaa4ca655e42b63d564e1eeefdd0dc2b9ac6cf
josephdprince <57415450+josephdprince@users.noreply.github.com>
2021-09-09 11:18:13 -0700

    Update WordLadder.cpp

--- WordLadder.cpp
+++ WordLadder.cpp
@@ -13,15 +13,18 @@ WordLadder::WordLadder(const string & filename) {
     string temp;
 
     try {
-        fin.open(filename); //opening file and making sure that it opens correctly
+        fin.open(filename);
         if (!fin.is_open()) {
             throw runtime_error("File could not be opened");
         }
-        while (fin >> temp) {  //inputting the dictionary into the list
-            if (temp.size() != 5) { //if one word is too large throw an exception
+        // Inputting the dictionary into the list
+        while (fin >> temp) {
+            // If one word is too large throw an exception
+            if (temp.size() != 5) {
                 throw range_error("Words in list must have exactly 5 characters");
             }
-            else { //otherwise put the word in the list
+            // Otherwise put the word in the list
+            else {
                 dict.push_back(temp);
             }
         }
@@ -39,19 +42,23 @@ WordLadder::WordLadder(const string & filename) {
 
 void WordLadder::outputLadder(const string &start, const string &end, const string &outputFile) {
     ofstream fout;
-    fout.open(outputFile); //opens an output file stream but doesnt check if it exists because it'll make one
+    fout.open(outputFile);
     
     try {
-        if (start == end) { //if the user inputs the same 2 words for the ladder
+        // If the user inputs the same 2 words for the ladder
+        if (start == end) {
             throw unexpected_handler();
         }
 
         int counter = 0;
-        for (list<string>::iterator it = dict.begin(); *it != dict.back(); ++it) { //iterating through the dictionary
-            if (start == *it || end == *it) { //use a counter to see if the word exists in the dictionary
+        // Iterate through the dictionary
+        for (list<string>::iterator it = dict.begin(); *it != dict.back(); ++it) {
+            // If we find either the start or end word
+            if (start == *it || end == *it) {
                 ++counter;
             }
-            if (counter == 2) { //if both words exist, find the word ladder
+            // If counter == 2 then both words exist in the dictionary. Now find word ladder
+            if (counter == 2) {
                 PlayWordLadder(start, end, outputFile, fout);
                 return;
             }
@@ -72,61 +79,62 @@ void WordLadder::PlayWordLadder(const string &start, const string &end, const st
     stack<string> s;
     queue<stack<string>> q;
 
-    s.push(start); //starts the first stack with just the word inputted by the user
-    q.push(s); //pushes that stack onto the queue
+    s.push(start);
+    q.push(s);
 
-    dict.remove(start); //removes the starting word from the dictionary so we don't use that as a similar word. 
+    // Remove starting word from dictionary since we will never need it
+    dict.remove(start);
 
     string tempWord;
     int diffCount = 0;
 
-    while (!q.empty()) {  //if the queue is emptied, that means that a word ladder was not found
-        tempWord = q.front().top(); //gets the word at the top of the first stack in the queue
+    // If the queue is emptied, that means that a word ladder was not found
+    while (!q.empty()) {
+        // Gets top of stack of the first queue. This is what we will compare with
+        tempWord = q.front().top();
         for (list<string>::iterator it = dict.begin(); it != dict.end(); ++it) { 
             diffCount = 0;
             unsigned i = 0;
             while (i < it->size()) {
-                if (optimization == true) { //Extra credit: if the user wants to turn on optimization
-                    if (diffCount > 1) { //If there are 2 or more differences when comparing words, stop comparing
-                        i = it->size() - 1; //this will end the loop (there is a ++i at the end so we say size - 1)
-                    }
-                    else if (it->at(i) != tempWord.at(i)) { //if there is a difference count it and count the comparison
-                        ++diffCount;
-                        ++comparisons;
-                    }
+                // If there are 2 or more differences when comparing words, stop comparing
+                if (diffCount > 1) {
+                    // Manually ends the loop
+                    i = it->size() - 1;
                 }
-                else { //if the user does not want to optimize the algorithm.
-                    if (it->at(i) != tempWord.at(i)) { //same comparison as before
-                        ++diffCount;
-                        ++comparisons;
-                    }
+                // Counting the differences at each index
+                else if (it->at(i) != tempWord.at(i)) {
+                    ++diffCount;
                 }
                 ++i;
             }
 
-            if (diffCount == 1) { //if the words are similar (off by just one letter)
+            // If the words are similar (off by just one letter)
+            if (diffCount == 1) { 
                 stack<string> newStack;
-                newStack = q.front(); //create a new stack with the same contents of the first stack in queue
-                newStack.push(*it); //push the similar word we found onto this newstack
-                if (newStack.top() == end) { //if that word we found was the end word then we have our word ladder
-                    printStack(newStack, fout); //prints the stack via file output
+                
+                // Create a new stack with the same contents of the first stack in queue
+                newStack = q.front();
+                // Push the similar word we found onto this newstack
+                newStack.push(*it);
+                
+                // If that word we found was the end word then we have our word ladder
+                if (newStack.top() == end) {
+                    printStack(newStack, fout);
                     fout.close();
                     return;
                 }
                 else {
-                    q.push(newStack); //if not the end of the ladder then just push the new stack onto the queue
-                    *it = ""; //deletes the word without causing us to lose out iterator position
+                    // Push the new stack with the word we found into the queue
+                    q.push(newStack);
+                    // Delete that word from the dictionary so we don't come back to it
+                    *it = "";
                 }
             }
         }
-        q.pop(); //pop the next stack in the queue
+        q.pop();
     }
         fout << "No Word Ladder Found." << endl;
         fout.close();
-}
-
-void WordLadder::setOptimization(bool b) {
-    optimization = b; //sets the optimization value
 }
 
 void printStack(stack<string> &ladder, ofstream &fout) {
